4393038f5d56524f30638957b9437687964d1fc6
qldo <idzhdl@gmail.com>
2026-04-24 15:47:22 +0900

    Initial setting for spring boot.

---
+++ src/main/java/com/michelet/timeslotservice/TimeslotserviceApplication.java
@@ -0,0 +1,13 @@
+package com.michelet.timeslotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeslotserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeslotserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelet/timeslotservice/TimeslotserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelet.timeslotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeslotserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
