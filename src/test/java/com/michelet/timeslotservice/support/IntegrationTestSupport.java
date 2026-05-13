c56a8c733662512f1088e9358e9c00298a4957fb
qldo <idzhdl@gmail.com>
2026-05-14 00:52:43 +0900

    test: Testcontainer 싱글톤 생성 및 integration 에 적용

---
+++ src/test/java/com/michelet/timeslotservice/support/IntegrationTestSupport.java
@@ -0,0 +1,27 @@
+package com.michelet.timeslotservice.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * 싱글톤 패턴을 이용한 PostgreSQL Testcontainer 사용
+ */
+@ActiveProfiles("local")
+@SpringBootTest()
+@Testcontainers
+public abstract class IntegrationTestSupport {
+
+    @SuppressWarnings("resource")
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("timeslot_test")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        postgresContainer.start();
+    }
+}
