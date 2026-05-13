edb7b5708e7e8da2477ea512c9ffefae714ee19d
qldo <idzhdl@gmail.com>
2026-05-13 15:12:33 +0900

    feat: 인원 복원하는 내부 API 엔드포인트 작성 및 Patch consistency 유지 및 time-slots consistency 유지

--- src/main/java/com/michelet/timeslotservice/presentation/controller/TimeSlotInternalController.java
+++ src/main/java/com/michelet/timeslotservice/presentation/controller/TimeSlotInternalController.java
@@ -2,8 +2,8 @@ package com.michelet.timeslotservice.presentation.controller;
 
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +12,13 @@ import com.michelet.common.response.ApiResponse;
 import com.michelet.timeslotservice.application.service.TimeSlotService;
 import com.michelet.timeslotservice.presentation.code.TimeSlotSuccessCode;
 import com.michelet.timeslotservice.presentation.dto.request.TimeSlotDeductCapacityRequest;
+import com.michelet.timeslotservice.presentation.dto.request.TimeSlotRestoreRequest;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/internal/v1/timeslots")
+@RequestMapping("/internal/v1/time-slots")
 @RequiredArgsConstructor
 public class TimeSlotInternalController {
 
@@ -30,7 +31,7 @@ public class TimeSlotInternalController {
      * @param request 차감 요청 정보 (인원 수)
      * @return 공통 응답 규격
      */    
-    @PostMapping("/{timeSlotId}/deduct")
+    @PatchMapping("/{timeSlotId}/deduct")
     public ApiResponse<Void> deductCapacity(
             @PathVariable UUID timeSlotId, 
             @Valid @RequestBody TimeSlotDeductCapacityRequest request) {
@@ -38,6 +39,23 @@ public class TimeSlotInternalController {
         timeSlotService.deductCapacity(timeSlotId, request.requiredCapacity());
         return ApiResponse.ok(TimeSlotSuccessCode.DEDUCT_SUCCESS, null);
         
+    }
+
+    /**
+     * 특정 타임슬롯의 예약 인원을 복원하는 내부 API 엔드포인트.
+     * @param timeSlotId
+     * @param request
+     * @return
+     */
+
+    @PatchMapping("/{timeSlotId}/restore")
+    public ApiResponse<Void> restoreTimeSlot(
+            @PathVariable UUID timeSlotId,
+            @RequestBody @Valid TimeSlotRestoreRequest request) {
+        
+        timeSlotService.restoreTimeSlot(timeSlotId, request.restoreCapacity());
+        
+        return ApiResponse.ok(TimeSlotSuccessCode.RESTORE_SUCCESS, null);
     }
 
 }
