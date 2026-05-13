dcda75d63ff74251b25f3fa2708c1189ab5cd141
qldo <idzhdl@gmail.com>
2026-05-14 01:32:05 +0900

    fix: @Testcontainer 로 .start() 과 중복되는 행위 삭제

--- src/test/java/com/michelet/timeslotservice/support/IntegrationTestSupport.java
+++ src/test/java/com/michelet/timeslotservice/support/IntegrationTestSupport.java
@@ -4,19 +4,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * 싱글톤 패턴을 이용한 PostgreSQL Testcontainer 사용
  */
 @ActiveProfiles("local")
 @SpringBootTest("eureka.client.enabled=false")
-@Testcontainers
 public abstract class IntegrationTestSupport {
 
     @SuppressWarnings("resource")
     @ServiceConnection
-    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:18")
             .withDatabaseName("timeslot_test")
             .withUsername("test")
             .withPassword("test");
