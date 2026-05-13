b570ae24776355e353a79b8ebb9d249723fc42da
qldo <idzhdl@gmail.com>
2026-05-13 15:46:30 +0900

    fix: OPENED 변경 이중 조건 추가

--- src/main/java/com/michelet/timeslotservice/domain/TimeSlot.java
+++ src/main/java/com/michelet/timeslotservice/domain/TimeSlot.java
@@ -123,7 +123,7 @@ public class TimeSlot {
         
         this.remainingCapacity += restoreCapacity;
 
-        if (this.remainingCapacity > 0) {
+        if (this.remainingCapacity > 0 && this.status == TimeSlotStatus.CLOSED) {
             this.status = TimeSlotStatus.OPENED;
         }
     }
