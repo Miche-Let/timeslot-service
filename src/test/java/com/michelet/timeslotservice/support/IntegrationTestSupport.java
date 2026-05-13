57a640be9f6fe4a776c27024581ce418419a7aaa
qldo <idzhdl@gmail.com>
2026-05-14 04:29:43 +0900

    test: 포트 랜덤 설정 및 secret key 를 이용한 jwt 자동 생성

--- src/test/java/com/michelet/timeslotservice/support/IntegrationTestSupport.java
+++ src/test/java/com/michelet/timeslotservice/support/IntegrationTestSupport.java
@@ -1,16 +1,59 @@
 package com.michelet.timeslotservice.support;
 
+import static org.mockito.BDDMockito.given;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.crypto.SecretKey;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 /**
  * 싱글톤 패턴을 이용한 PostgreSQL Testcontainer 사용
  */
 @ActiveProfiles("local")
-@SpringBootTest("eureka.client.enabled=false")
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, 
+    properties = {"eureka.client.enabled=false"}
+)
 public abstract class IntegrationTestSupport {
+
+    @MockitoBean
+    protected AuditorAware<UUID> auditorAware; 
+
+    @BeforeEach
+    void setUpAuditor() {
+        given(auditorAware.getCurrentAuditor()).willReturn(Optional.of(UUID.randomUUID()));
+    }
+
+
+    @Value("${internal.auth.secret}")
+    private String secretKey;
+
+    protected String createTestInternalToken() {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .issuer("michelet-admin")
+                .audience().add("timeslot-service").and()
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+                .signWith(key, Jwts.SIG.HS256)
+                .compact();
+    }
 
     @SuppressWarnings("resource")
     @ServiceConnection
